99eb45bab88773bef1bc66e117ccad0ab42b9ed3
Carol0303 <alsgmlqkd@naver.com>
2020-01-14 15:19:12 +0900

    second commit in branch

--- TestClass.java
+++ TestClass.java
@@ -3,7 +3,8 @@ package test;
 public class TestClass {
     
     public static void main (String[] args) {
-        System.out.println("Hi!!");
+        System.out.println("Hi!");
+        System.out.println("Welcome!");
     }
 
 }
