e5bc96bccbf5f65fae2fa5bac06e0891822d6207
Carol0303 <alsgmlqkd@naver.com>
2020-01-14 16:17:52 +0900

    local merge

--- TestClass.java
+++ TestClass.java
@@ -4,11 +4,7 @@ public class TestClass {
     
     public static void main (String[] args) {
         System.out.println("Hi!!");
-<<<<<<< HEAD
         System.out.println("Welcome~~~!");
-=======
-        System.out.println("Welcome!!!");
->>>>>>> remotes/origin/master
     }
 
 }
